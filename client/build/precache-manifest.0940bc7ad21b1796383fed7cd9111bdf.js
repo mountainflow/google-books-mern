5bb386393984a02e61a98a9804530dd78b0f2f80
mountainflow <46331541+mountainflow@users.noreply.github.com>
2019-07-05 15:01:44 -0600

    added build

---
+++ client/build/precache-manifest.0940bc7ad21b1796383fed7cd9111bdf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "235edc075cf63a3a5a28",
+    "url": "/static/js/main.99d40a52.chunk.js"
+  },
+  {
+    "revision": "c017f654936c8132efa9",
+    "url": "/static/js/2.ddcd2c10.chunk.js"
+  },
+  {
+    "revision": "235edc075cf63a3a5a28",
+    "url": "/static/css/main.7c536fdd.chunk.css"
+  },
+  {
+    "revision": "c017f654936c8132efa9",
+    "url": "/static/css/2.a7310e84.chunk.css"
+  },
+  {
+    "revision": "eb37873e7cd2924a06f26da393a233a9",
+    "url": "/index.html"
+  }
+];
